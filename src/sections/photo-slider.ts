b6b517aa12a9d8a6ee7ae06614e8cd106e8f1fc1
jm <jm@blindcreative.com>
2025-01-26 18:18:09 -0700

    photo slider!

--- src/sections/photo-slider.ts
+++ src/sections/photo-slider.ts
@@ -1,16 +1,40 @@
 import Swiper from 'swiper';
+import { Pagination, Navigation } from 'swiper/modules';
 
 
 // init Swiper:
 const swiperImages = new Swiper('#photo-slider--images', {
   slidesPerView: 1.2,
+  // loop: true,
+  on: {
+    slideChange: (swiper) => {
+      swiperText?.slideTo(swiper.activeIndex);
+    },
+  },
   breakpoints: {
     1024: {
       slidesPerView: 1.6,
+      allowTouchMove: false,
+      preventClicks: true,
     },
   },
 });
 
 const swiperText = new Swiper('#photo-slider--text', {
   slidesPerView: 1,
+  // loop: true,
+  modules: [Pagination, Navigation],
+  pagination: {
+    el: '.swiper-pagination--text',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next--text',
+    prevEl: '.swiper-button-prev--text',
+  },
+  on: {
+    slideChange: (swiper) => {
+      swiperImages.slideTo(swiper.activeIndex);
+    },
+  },
 });
