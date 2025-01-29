4286425075a80a90ce771e77a2e6f95ded9ed1b1
jm <jm@blindcreative.com>
2025-01-28 22:29:39 -0700

    sdfa

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{js,ts,jsx,tsx,css}',
     './sections/*.liquid',
     './snippets/*.liquid',
     './layout/*.liquid',
@@ -25,6 +25,13 @@ module.exports = {
       spacing: {
         50: '50px',
         100: '100px',
+      },
+      colors: {
+        'dark-grey': 'rgba(0, 0, 0, 0.75)',
+        'grey-50': 'rgba(0, 0, 0, 0.5)',
+        border: 'rgba(#4C4C46, 0.25)',
+        'coratina-green': '#4C601B',
+        'picual-red': '#C73A15',
       },
     },
   },
