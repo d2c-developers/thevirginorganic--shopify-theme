5420ae9bef59176b4afbc72c4f362a148db5fcd5
jm <jm@blindcreative.com>
2025-01-31 01:35:49 -0700

    shop now fixed button

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,6 +29,7 @@ module.exports = {
       colors: {
         'dark-grey': 'rgba(0, 0, 0, 0.75)',
         'grey-50': 'rgba(0, 0, 0, 0.5)',
+        'grey-25': 'rgba(0, 0, 0, 0.25)',
         border: 'rgba(#4C4C46, 0.25)',
         'coratina-green': '#4C601B',
         'picual-red': '#C73A15',
